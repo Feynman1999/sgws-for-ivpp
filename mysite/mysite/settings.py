275515af036f2414538390dd7cf35e74cacf1882
feynman1999 <chenyx.cs@gmail.com>
2019-12-16 23:18:01 +0800

    base navbar and footer

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'comment',
+    'ivp',
     'user',
+    'comment',
 ]
 
 MIDDLEWARE = [
